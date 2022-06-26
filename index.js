111a0459da1726e0e7110520698c2c6c127c1f79
Satwik Das <satwikdas4@gmail.com>
2022-06-27 04:01:30 +0530

    Added Login page and Main about page Structure.

---
+++ index.js
@@ -0,0 +1,7 @@
+const f = document.querySelector(form);
+
+const inButton = document.querySelector('#login')
+inButton.addEventListener("click", () => { })    //complete sign in button
+ 
+const upButton = document.querySelector('#signup')
+upButton.addEventListener("click", () => { })   //complete sign up button
